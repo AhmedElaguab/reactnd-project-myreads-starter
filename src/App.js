af7e901968ab703b660426a14ed1db716dc003ed
AhmedElaguab <elaguabahmed97@gmail.com>
2019-01-07 22:19:23 +0100

    fix: Use React Router instead of State value change

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Route } from "react-router-dom";
 import * as BooksAPI from "./BooksAPI";
 import SearchBooks from "./components/SearchBooks";
 import ListBooks from "./components/ListBooks";
@@ -6,21 +7,14 @@ import "./App.css";
 
 class BooksApp extends React.Component {
   state = {
-    books: [],
-
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: false
+    books: []
   };
 
   render() {
     return (
       <div className="app">
-        {this.state.showSearchPage ? <SearchBooks /> : <ListBooks />}
+        <Route exact path="/" render={() => <ListBooks />} />
+        <Route path="/search" render={() => <SearchBooks />} />
       </div>
     );
   }
