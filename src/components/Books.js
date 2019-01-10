bff1ce58332a28694f550e89de1e4eb174ecf471
AhmedElaguab <elaguabahmed97@gmail.com>
2019-01-10 23:16:40 +0100

    fix: Check if there are books before show them

--- src/components/Books.js
+++ src/components/Books.js
@@ -36,44 +36,50 @@ class Books extends Component {
   render() {
     return (
       <ol className="books-grid">
-        {this.props.books.map(book => (
-          <li key={book.id}>
-            <div className="book">
-              <div className="book-top">
-                <div
-                  className="book-cover"
-                  style={{
-                    width: 128,
-                    height: 193,
-                    backgroundImage: `url(${book.imageLinks.smallThumbnail})`
-                  }}
-                />
-                <div className="book-shelf-changer">
-                  <select
-                    value={book.shelf}
-                    onChange={event =>
-                      this.props.onShelfSelected(event.target.value, book)
-                    }
-                  >
-                    <option value="move" disabled>
-                      Move to...
-                    </option>
-                    <option value="currentlyReading">Currently Reading</option>
-                    <option value="wantToRead">Want to Read</option>
-                    <option value="read">Read</option>
-                    <option value="none">None</option>
-                  </select>
+        {this.props.books.length > 0 ? (
+          this.props.books.map(book => (
+            <li key={book.id}>
+              <div className="book">
+                <div className="book-top">
+                  <div
+                    className="book-cover"
+                    style={{
+                      width: 128,
+                      height: 193,
+                      backgroundImage: `url(${book.imageLinks.smallThumbnail})`
+                    }}
+                  />
+                  <div className="book-shelf-changer">
+                    <select
+                      value={book.shelf}
+                      onChange={event =>
+                        this.props.onShelfSelected(event.target.value, book)
+                      }
+                    >
+                      <option value="move" disabled>
+                        Move to...
+                      </option>
+                      <option value="currentlyReading">
+                        Currently Reading
+                      </option>
+                      <option value="wantToRead">Want to Read</option>
+                      <option value="read">Read</option>
+                      <option value="none">None</option>
+                    </select>
+                  </div>
+                </div>
+                <div className="book-title">{book.title}</div>
+                <div className="book-authors">
+                  {book.authors.map(author => (
+                    <p key={author}>{author}</p>
+                  ))}
                 </div>
               </div>
-              <div className="book-title">{book.title}</div>
-              <div className="book-authors">
-                {book.authors.map(author => (
-                  <p key={author}>{author}</p>
-                ))}
-              </div>
-            </div>
-          </li>
-        ))}
+            </li>
+          ))
+        ) : (
+          <p>No books :[</p>
+        )}
       </ol>
     );
   }
