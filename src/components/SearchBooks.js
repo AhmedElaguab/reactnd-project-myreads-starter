32ba0255055a4e4320fbeb1c62e2238d72aba067
AhmedElaguab <elaguabahmed97@gmail.com>
2019-01-14 23:52:12 +0100

    fix: Add propTypes to the search page

--- src/components/SearchBooks.js
+++ src/components/SearchBooks.js
@@ -1,9 +1,14 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
 import * as BooksAPI from "../BooksAPI";
 import Books from "./Books";
 
 class SearchBooks extends Component {
+  static propTypes = {
+    onShelfSelected: PropTypes.func.isRequired
+  };
+
   state = {
     query: "",
     searchResultBooks: [],
