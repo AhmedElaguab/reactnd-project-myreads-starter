954125cb6405ee9aa6d0c39b7d384a63024a1ead
AhmedElaguab <elaguabahmed97@gmail.com>
2019-01-14 20:23:50 +0100

    fix: Fix multiple 'value' input value bug

--- src/components/Books.js
+++ src/components/Books.js
@@ -11,49 +11,53 @@ class Books extends Component {
     return (
       <ol className="books-grid">
         {this.props.books.length > 0 ? (
-          this.props.books.map(book => (
-            <li key={book.id}>
-              <div className="book">
-                <div className="book-top">
-                  <div
-                    className="book-cover"
-                    style={{
-                      width: 128,
-                      height: 193,
-                      backgroundImage: `url(${book.imageLinks.smallThumbnail})`
-                    }}
-                  />
-                  <div className="book-shelf-changer">
-                    <select
-                      value={book.shelf}
-                      defaultValue="none"
-                      onChange={event =>
-                        this.props.onShelfSelected(event.target.value, book)
-                      }
-                    >
-                      <option value="move" disabled>
-                        Move to...
-                      </option>
-                      <option value="currentlyReading">
-                        Currently Reading
-                      </option>
-                      <option value="wantToRead">Want to Read</option>
-                      <option value="read">Read</option>
-                      <option value="none">None</option>
-                    </select>
+          this.props.books.map(book => {
+            const shelf = book.shelf ? book.shelf : "none";
+            return (
+              <li key={book.id}>
+                <div className="book">
+                  <div className="book-top">
+                    <div
+                      className="book-cover"
+                      style={{
+                        width: 128,
+                        height: 193,
+                        backgroundImage: `url(${
+                          book.imageLinks.smallThumbnail
+                        })`
+                      }}
+                    />
+                    <div className="book-shelf-changer">
+                      <select
+                        value={shelf}
+                        onChange={event =>
+                          this.props.onShelfSelected(event.target.value, book)
+                        }
+                      >
+                        <option value="move" disabled>
+                          Move to...
+                        </option>
+                        <option value="currentlyReading">
+                          Currently Reading
+                        </option>
+                        <option value="wantToRead">Want to Read</option>
+                        <option value="read">Read</option>
+                        <option value="none">None</option>
+                      </select>
+                    </div>
+                  </div>
+                  <div className="book-title">{book.title}</div>
+                  <div className="book-authors">
+                    {book.authors ? (
+                      book.authors.map(author => <p key={author}>{author}</p>)
+                    ) : (
+                      <p>Unknown author</p>
+                    )}
                   </div>
                 </div>
-                <div className="book-title">{book.title}</div>
-                <div className="book-authors">
-                  {book.authors ? (
-                    book.authors.map(author => <p key={author}>{author}</p>)
-                  ) : (
-                    <p>Unknown author</p>
-                  )}
-                </div>
-              </div>
-            </li>
-          ))
+              </li>
+            );
+          })
         ) : (
           <p>No books :[</p>
         )}
