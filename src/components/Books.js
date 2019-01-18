1f6e56a48d01f90250963a568823f50aac60f5c5
AhmedElaguab <elaguabahmed97@gmail.com>
2019-01-18 16:38:31 +0100

    fix: Use function component instead of class component

--- src/components/Books.js
+++ src/components/Books.js
@@ -1,93 +1,89 @@
-import React, { Component } from "react";
+import React from "react";
 import PropTypes from "prop-types";
 
-class Books extends Component {
-  static propTypes = {
-    books: PropTypes.array.isRequired,
-    onShelfSelected: PropTypes.func
-  };
-
-  render() {
-    return (
-      <ol className="books-grid">
-        {this.props.books.length > 0 ? (
-          this.props.books.map(book => {
-            const shelf = book.shelf ? book.shelf : "none";
-            const smallThumbnail =
-              book.imageLinks && book.imageLinks.smallThumbnail
-                ? book.imageLinks.smallThumbnail
-                : "";
-            return (
-              <li key={book.id}>
-                <div className="book">
-                  <div className="book-thumb">
-                    <div
-                      className="book-cover"
-                      style={{
-                        width: 128,
-                        height: 193,
-                        backgroundImage: `url(${smallThumbnail})`
-                      }}
-                    />
-                    <div className="book-shelf-changer">
-                      <select
-                        value={shelf}
-                        onChange={event =>
-                          this.props.onShelfSelected(event.target.value, book)
-                        }
-                      >
-                        <option value="move" disabled>
-                          Move to...
-                        </option>
-                        <option value="currentlyReading">
-                          Currently Reading
-                        </option>
-                        <option value="wantToRead">Want to Read</option>
-                        <option value="read">Read</option>
-                        <option value="none">None</option>
-                      </select>
-                    </div>
-                  </div>
-                  <div className="book-details">
-                    <h2
-                      className="book-title"
-                      title={book.subtitle ? book.subtitle : ""}
+const Books = props => {
+  return (
+    <ol className="books-grid">
+      {props.books.length > 0 ? (
+        props.books.map(book => {
+          const shelf = book.shelf ? book.shelf : "none";
+          const smallThumbnail =
+            book.imageLinks && book.imageLinks.smallThumbnail
+              ? book.imageLinks.smallThumbnail
+              : "";
+          return (
+            <li key={book.id}>
+              <div className="book">
+                <div className="book-thumb">
+                  <div
+                    className="book-cover"
+                    style={{
+                      width: 128,
+                      height: 193,
+                      backgroundImage: `url(${smallThumbnail})`
+                    }}
+                  />
+                  <div className="book-shelf-changer">
+                    <select
+                      value={shelf}
+                      onChange={event =>
+                        props.onShelfSelected(event.target.value, book)
+                      }
                     >
-                      {book.title}
-                    </h2>
-                    <p>
-                      <span>{book.publishedDate}</span> |{" "}
-                      <span>{book.pageCount} pages</span>
-                    </p>
-                    <ul className="book-authors">
-                      {book.authors ? (
-                        book.authors.map(author => (
-                          <li key={author}>{author}</li>
-                        ))
-                      ) : (
-                        <li>Unknown author</li>
-                      )}
-                    </ul>
-                    <ul className="book-categories">
-                      {book.categories ? (
-                        book.categories.map(categorie => (
-                          <li key={categorie}>{categorie}</li>
-                        ))
-                      ) : (
-                        <li>Unknown categories</li>
-                      )}
-                    </ul>
+                      <option value="move" disabled>
+                        Move to...
+                      </option>
+                      <option value="currentlyReading">
+                        Currently Reading
+                      </option>
+                      <option value="wantToRead">Want to Read</option>
+                      <option value="read">Read</option>
+                      <option value="none">None</option>
+                    </select>
                   </div>
                 </div>
-              </li>
-            );
-          })
-        ) : (
-          <p>No books :[</p>
-        )}
-      </ol>
-    );
-  }
-}
+                <div className="book-details">
+                  <h2
+                    className="book-title"
+                    title={book.subtitle ? book.subtitle : ""}
+                  >
+                    {book.title}
+                  </h2>
+                  <p>
+                    <span>{book.publishedDate}</span> |{" "}
+                    <span>{book.pageCount} pages</span>
+                  </p>
+                  <ul className="book-authors">
+                    {book.authors ? (
+                      book.authors.map(author => <li key={author}>{author}</li>)
+                    ) : (
+                      <li>Unknown author</li>
+                    )}
+                  </ul>
+                  <ul className="book-categories">
+                    {book.categories ? (
+                      book.categories.map(categorie => (
+                        <li key={categorie}>{categorie}</li>
+                      ))
+                    ) : (
+                      <li>Unknown categories</li>
+                    )}
+                  </ul>
+                </div>
+              </div>
+            </li>
+          );
+        })
+      ) : (
+        <p>No books :[</p>
+      )}
+    </ol>
+  );
+};
+
+Books.propTypes = {
+  books: PropTypes.array.isRequired,
+  onShelfSelected: PropTypes.func
+};
 
 export default Books;
